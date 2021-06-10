9f292da9c933d9f720e732c56cf9d896ac821812
mathieuBelliveau <36576490+mathieuBelliveau@users.noreply.github.com>
2021-06-09 21:47:07 -0300

    Add project files.

---
+++ StackLayoutExercise/StackLayoutExercise/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StackLayoutExercise
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //-1,0,+1:round down, none, round up
+        private void CalculateTip(int rounding)
+        {
+            if (rounding > 1 || rounding < -1)
+                return;
+
+            double t;
+            if (Double.TryParse(input.Text, out t) && t > 0)
+            {
+
+            }
+        }
+    }
+}
