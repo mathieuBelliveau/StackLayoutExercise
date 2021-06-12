4b43ab0935777a7920f3b922c410ca99651b5707
mathieuBelliveau <36576490+mathieuBelliveau@users.noreply.github.com>
2021-06-11 21:18:23 -0300

    Implemented backend function: - listeners - Calculation method

--- StackLayoutExercise/StackLayoutExercise/MainPage.xaml.cs
+++ StackLayoutExercise/StackLayoutExercise/MainPage.xaml.cs
@@ -13,17 +13,49 @@ namespace StackLayoutExercise
         public MainPage()
         {
             InitializeComponent();
-        }
-        //-1,0,+1:round down, none, round up
-        private void CalculateTip(int rounding)
-        {
-            if (rounding > 1 || rounding < -1)
-                return;
+            up.CheckedChanged += (s, e) => CalculateTip();
+            down.CheckedChanged += (s, e) => CalculateTip();
+            input.TextChanged += (s, e) => CalculateTip();
 
-            double t;
-            if (Double.TryParse(input.Text, out t) && t > 0)
+            fif.Clicked += (s, e) => { slide.Value = 15; };
+            twen.Clicked += (s, e) => { slide.Value = 20; };
+
+            slide.ValueChanged += (s, e) =>
             {
+                double pct = Math.Round(e.NewValue);
+                percent.Text = pct + "%";
+                CalculateTip();
+            };
+        }
 
+
+        //round up for positive, round down for negative
+        private void CalculateTip()
+        {
+            bool upCheck = up.IsChecked;
+            bool downCheck = down.IsChecked;
+            double pct = Math.Round(slide.Value);
+            double tipp;
+            double totall;
+            double bill;
+            if (Double.TryParse(input.Text, out bill) && bill > 0)
+            {
+                tipp = Math.Round(bill * (pct/100),2);
+                totall = bill + tipp;
+
+                if (upCheck && !downCheck)
+                {
+                    totall = Math.Ceiling(totall);
+
+                }
+
+                else if (!upCheck && downCheck)
+                {
+                    totall = Math.Floor(totall);
+                }
+
+                tip.Text = tipp.ToString("C");
+                total.Text = totall.ToString("C");
             }
         }
     }
